7c9ad391b78322117603fc0d3ac3aea9245b8fae
Cegaje <Cegaje17@gmail.com>
2021-01-04 17:11:00 -0600

    estructura proyecto

---
+++ src/main/java/com/untels/estadonutricional/EstadonutricionalApplication.java
@@ -0,0 +1,13 @@
+package com.untels.estadonutricional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstadonutricionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstadonutricionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/untels/estadonutricional/EstadonutricionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.untels.estadonutricional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstadonutricionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
