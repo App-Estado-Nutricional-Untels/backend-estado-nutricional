0f56248a526d97c8d34d2fa83de87b97821bec35
jibaru <ignacioruedaboada@hotmail.com>
2021-01-05 12:18:13 -0500

    Creados DTO de respuesta Se crearon los DTO Respuesta, RespuestaError y Error

---
+++ src/main/java/com/untels/estadonutricional/dto/response/RespuestaError.java
@@ -0,0 +1,35 @@
+package com.untels.estadonutricional.dto.response;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RespuestaError {
+
+    private boolean ok = false;
+    private List<Error> errores;
+
+    public RespuestaError(Error... errores) {
+        this.errores = Arrays.asList(errores);
+    }
+
+    public RespuestaError(List<Error> errores) {
+        this.errores = errores;
+    }
+
+    public boolean isOk() {
+        return ok;
+    }
+
+    public void setOk(boolean ok) {
+        this.ok = ok;
+    }
+
+    public List<Error> getErrores() {
+        return errores;
+    }
+
+    public void setErrores(List<Error> errores) {
+        this.errores = errores;
+    }
+
+}
