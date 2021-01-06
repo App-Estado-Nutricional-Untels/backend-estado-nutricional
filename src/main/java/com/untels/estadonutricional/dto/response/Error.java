aa9652d282d694ef0821f502a4f0ec3e98a66f45
Ignacio <ignacioruedaboada@hotmail.com>
2021-01-05 21:21:13 -0500

    Agregado campo de nombre campo Se agregó el campo de nombre campo al Error de respuestas

--- src/main/java/com/untels/estadonutricional/dto/response/Error.java
+++ src/main/java/com/untels/estadonutricional/dto/response/Error.java
@@ -2,10 +2,20 @@ package com.untels.estadonutricional.dto.response;
 
 public class Error {
 
+    private String campo;
     private String mensaje;
 
-    public Error(String mensaje) {
+    public Error(String campo, String mensaje) {
+        this.campo = campo;
         this.mensaje = mensaje;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
     }
 
     public String getMensaje() {
