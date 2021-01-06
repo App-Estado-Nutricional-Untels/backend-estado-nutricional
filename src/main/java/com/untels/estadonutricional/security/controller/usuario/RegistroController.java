62d4f280209cca0082aa4937528ae55fdb022bf4
Cegaje <Cegaje17@gmail.com>
2021-01-06 17:06:32 -0600

    Controlador Registro Servicio que permite registrar alumno o medico, culminado. close #6

---
+++ src/main/java/com/untels/estadonutricional/security/controller/usuario/RegistroController.java
@@ -0,0 +1,169 @@
+
+package com.untels.estadonutricional.security.controller.usuario;
+
+import com.untels.estadonutricional.dto.response.Error;
+import com.untels.estadonutricional.dto.response.Respuesta;
+import com.untels.estadonutricional.dto.response.RespuestaError;
+import com.untels.estadonutricional.enums.SexoNombre;
+import com.untels.estadonutricional.security.dto.request.RegistroBody;
+import com.untels.estadonutricional.security.entity.Usuario;
+import com.untels.estadonutricional.security.enums.RolNombre;
+import com.untels.estadonutricional.security.service.NuevoUsuarioService;
+import com.untels.estadonutricional.security.service.UsuarioService;
+import com.untels.estadonutricional.service.AlumnoService;
+import com.untels.estadonutricional.service.CodigoMedValidoService;
+import com.untels.estadonutricional.service.CodigoUniValidoService;
+import com.untels.estadonutricional.service.MedicoService;
+import com.untels.estadonutricional.service.PersonaService;
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin
+@Validated
+public class RegistroController {
+    
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    private PersonaService personaService;
+
+    @Autowired
+    private MedicoService medicoService;
+
+    @Autowired
+    private AlumnoService alumnoService;
+
+    @Autowired
+    private CodigoMedValidoService codigoMedValidoService;
+
+    @Autowired
+    private CodigoUniValidoService codigoUniValidoService;
+
+    @Autowired
+    NuevoUsuarioService nuevoUsuarioService;
+
+    @PostMapping("/registro")
+    public ResponseEntity<?> nuevoUsuario(
+            @Valid @RequestBody RegistroBody registroBody
+    ) {
+        // Validación de errores de duplicación
+        List<Error> errores = getErrores(registroBody);
+
+        if (!errores.isEmpty()) {
+            return new ResponseEntity<>(
+                    new RespuestaError(errores),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        RolNombre rolNombre = getRolNombre(registroBody.getRol());
+
+        // Guardado de usuario, persona y opcionalmente médico o alumno
+        Usuario usuario = nuevoUsuarioService.crearUsuarioCompleto(
+                new NuevoUsuarioService.UsuarioCompleto(
+                        registroBody.getNombres(),
+                        registroBody.getApePaterno(),
+                        registroBody.getApeMaterno(),
+                        registroBody.getDni(),
+                        registroBody.getCorreoElectronico(),
+                        registroBody.getCodigo(),
+                        null,
+                        (registroBody.getSexo().equalsIgnoreCase("F")
+                        ? SexoNombre.F
+                        : SexoNombre.M),
+                        rolNombre,
+                        registroBody.getClave()));
+
+        return new ResponseEntity(new Respuesta(
+                usuario,
+                "Usuario completo creado"),
+                HttpStatus.OK);
+    }
+
+    private List<com.untels.estadonutricional.dto.response.Error> getErrores(RegistroBody registroBody) {
+        List<com.untels.estadonutricional.dto.response.Error> errores = new ArrayList<>();
+
+        if (usuarioService.existePorCorreoElectronico(
+                registroBody.getCorreoElectronico()
+        )) {
+            errores.add(new com.untels.estadonutricional.dto.response.Error(
+                    "correoElectronico",
+                    "el correo electrónico ya existe"));
+        }
+
+        if (personaService.existePorDni(
+                registroBody.getDni()
+        )) {
+            errores.add(new com.untels.estadonutricional.dto.response.Error(
+                    "dni",
+                    "el dni ya existe"));
+        }
+
+        if (registroBody.getRol().equalsIgnoreCase("ALUMNO")
+                || registroBody.getRol().equalsIgnoreCase("MEDICO")) {
+            if (registroBody.getCodigo() == null) {
+                errores.add(new com.untels.estadonutricional.dto.response.Error(
+                        "codigo",
+                        "el codigo no puede ser nulo"));
+            } else {
+                if (registroBody.getRol().equalsIgnoreCase("ALUMNO")) {
+                    if (alumnoService.existePorCodigoUniversitario(
+                            registroBody.getCodigo()
+                    )) {
+                        errores.add(new com.untels.estadonutricional.dto.response.Error(
+                                "codigo",
+                                "el codigo ya existe"));
+                    }
+
+                    if (!codigoUniValidoService.esValido(
+                            registroBody.getCodigo()
+                    )) {
+                        errores.add(new com.untels.estadonutricional.dto.response.Error(
+                                "codigo",
+                                "el codigo no está permitido"));
+                    }
+                } else if (registroBody.getRol().equalsIgnoreCase("MEDICO")) {
+                    if (medicoService.existePorCodigoMedico(
+                            registroBody.getCodigo()
+                    )) {
+                        errores.add(new com.untels.estadonutricional.dto.response.Error(
+                                "codigo",
+                                "el codigo ya existe"));
+                    }
+
+                    if (!codigoMedValidoService.esValido(
+                            registroBody.getCodigo()
+                    )) {
+                        errores.add(new com.untels.estadonutricional.dto.response.Error(
+                                "codigo",
+                                "el codigo no está permitido"));
+                    }
+                }
+            }
+        }
+
+        return errores;
+    }
+
+    private RolNombre getRolNombre(String nombre) {
+        if (nombre.equalsIgnoreCase("alumno")) {
+            return RolNombre.ROLE_ALUMNO;
+        }
+
+        return RolNombre.ROLE_MEDICO;
+
+    }
+
+}
---
+++ src/main/java/com/untels/estadonutricional/security/dto/request/RegistroBody.java
@@ -0,0 +1,126 @@
+
+package com.untels.estadonutricional.security.dto.request;
+
+import com.untels.estadonutricional.utils.RegExpPatterns;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class RegistroBody {
+    
+    @NotBlank
+    private String nombres;
+
+    @NotBlank
+    private String apePaterno;
+
+    @NotBlank
+    private String apeMaterno;
+
+    @NotBlank
+    @Email
+    private String correoElectronico;
+
+    @NotBlank
+    @Size(min = 8, max = 8)
+    private String dni;
+
+    @NotBlank
+    @Pattern(
+            regexp = "F|M",
+            flags = Pattern.Flag.CASE_INSENSITIVE
+    )
+    private String sexo;
+
+    @NotBlank
+    @Pattern(
+            regexp = "ALUMNO|MEDICO",
+            flags = Pattern.Flag.CASE_INSENSITIVE
+    )
+    private String rol;
+
+    @Size(min = 10)
+    private String codigo;
+
+    @NotBlank
+    @Size(min = 6)
+    @Pattern(
+            regexp = RegExpPatterns.CLAVE,
+            message = "clave incorrecta"
+    )
+    private String clave;
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApePaterno() {
+        return apePaterno;
+    }
+
+    public void setApePaterno(String apePaterno) {
+        this.apePaterno = apePaterno;
+    }
+
+    public String getApeMaterno() {
+        return apeMaterno;
+    }
+
+    public void setApeMaterno(String apeMaterno) {
+        this.apeMaterno = apeMaterno;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+}
