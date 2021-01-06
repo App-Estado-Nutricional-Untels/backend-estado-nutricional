23fbc5d6c91eaa873265281a498650f70792c23c
Ignacio <ignacioruedaboada@hotmail.com>
2021-01-06 12:00:00 -0500

    SQL de inicio SQL para poder empezar a agregar usuarios

---
+++ sql_inicio.sql
@@ -0,0 +1,18 @@
+INSERT INTO `rol` (`
+rol_nombre`)
+VALUES
+    ('ROLE_ADMINISTRADOR'),
+    ('ROLE_MEDICO'),
+    ('ROLE_ALUMNO');
+
+INSERT INTO `codigo_medico_valido` (`
+codigo_medico`)
+VALUES
+    ('1111111111'),
+    ('2222222222');
+
+INSERT INTO `codigo_uni_valido` (`
+codigo_universitario`)
+VALUES
+    ('1111111111'),
+    ('2222222222');
