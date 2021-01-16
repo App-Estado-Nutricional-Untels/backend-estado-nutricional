58ad3730caffa9863dd4318d1fa56892f753a98f
jibaru <ignacioruedaboada@hotmail.com>
2021-01-16 11:25:53 -0500

    Corregida validación clave de usuario Se corrigió la validación para números y letras close #31

--- src/main/java/com/untels/estadonutricional/utils/RegExpPatterns.java
+++ src/main/java/com/untels/estadonutricional/utils/RegExpPatterns.java
@@ -5,5 +5,5 @@ public abstract class RegExpPatterns {
     public static final String FECHA
             = "^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$";
 
-    public static final String CLAVE = "[a-zA-Z0-9]*";
+    public static final String CLAVE = "^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]+$";
 }
