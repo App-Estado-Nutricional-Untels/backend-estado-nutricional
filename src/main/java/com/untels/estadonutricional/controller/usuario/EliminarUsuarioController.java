eb552e61e7debfbb890f6ca2588e7b0d87ba8a61
Cegaje <Cegaje17@gmail.com>
2021-01-16 16:05:52 -0600

    Correcion del MethodMapping en EliminarUsuarioController

--- src/main/java/com/untels/estadonutricional/controller/usuario/EliminarUsuarioController.java
+++ src/main/java/com/untels/estadonutricional/controller/usuario/EliminarUsuarioController.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +23,7 @@ public class EliminarUsuarioController {
     UsuarioService usuarioService;
     
     @PreAuthorize("hasRole('ADMINISTRADOR')")
-    @PostMapping("/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> eliminarUsuario(
             @PathVariable("id") int id
     ){
